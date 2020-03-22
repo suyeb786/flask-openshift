9c3a24f383b7fb453ef846bce88662f047926ea8
Suyeb Ansari <>
2020-03-22 10:30:41 +0530

    removed code

--- index.py
+++ index.py
@@ -1,18 +1,18 @@
 from flask import Flask
-from flask_api import status
+#from flask_api import status
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Index!", status.HTTP_404_NOT_FOUND
+    return "Index!"
 
 @app.route("/hello")
 def hello():
-    return "Hello World!", status.HTTP_200_OK
+    return "Hello World!"
     
 @app.route('/hello/<name>')
 def name(name):
-    return "Hello "+name, status.HTTP_202_ACCEPTED 
+    return "Hello "+name
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
